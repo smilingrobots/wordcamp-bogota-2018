8874826e8da7659a32c5fb19ed3b601907e0bd46
Willington Vega <wvega@wvega.com>
2018-09-03 15:36:19 -0500

    Agrega archivo principal con la cabecera del plugin.

---
+++ wordcamp-bogota-2018.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: WordCamp Bogotá 2018
+ * Plugin URI: https://github.com/smilingrobots/wordcamp-bogota-2018
+ * Description: Plugin de prueba para el Taller de introducción al desarrollo de plugins para WordPress.
+ * Version: 1.0.0
+ */
