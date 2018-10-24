b3cfea909532999e7ab5b7619ddd5f517c2662a9
Jorge A. Torres <j@jorgetorres.co>
2018-10-24 20:16:23 -0300

    Agregar un filtro para modificar el contenido del post insertando un mensaje al inicio

--- wcbog2018.php
+++ wcbog2018.php
@@ -5,3 +5,17 @@
  * Description: Plugin de prueba para el Taller de introducción al desarrollo de plugins para WordPress.
  * Version: 1.0
  */
+
+function wcbog2018_add_like_box_to_content( $content ) {
+    if ( ! is_singular() ) {
+        return $content;
+    }
+
+    $like_box  = '';
+    $like_box .= '<p>';
+    $like_box .= '-- Mi like box --';
+    $like_box .= '</p>';
+
+    return $like_box . $content;
+}
+add_filter( 'the_content', 'wcbog2018_add_like_box_to_content' );
